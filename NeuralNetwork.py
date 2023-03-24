df1716a38438149d7950119beeedf86d170f51a8
Wentao Shen <wshe801@aucklanduni.ac.nz>
2023-03-24 23:45:29 +1300

    code

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,52 +1,99 @@
 from tensorflow import keras
 import numpy as np
 
-
+class InputLayer:
+    def __init__(self, dim):
+        self.dim = dim
+        self.livelyNode = list(range(dim))
+    def activeNode(self, nodeIndex):
+        self.livelyNode = nodeIndex
+        return self
+    def input(self, data):
+        if(data.shape[1] != self.dim):
+            return None
+        else:
+            self.X = data
+            return self
+    def forwardPropogation(self):
+        self.Y = self.X[:,self.livelyNode]
+        return self
+    def output(self):
+        return self.Y
+class Layer:
+    def __init__(self, inDim, outDim, learning_rate):
+        self.inDim = inDim
+        self.outDim = outDim
+        self.learning_rate = learning_rate
+        self.livelyNode = list(range(outDim))
+    def initializeWeight(self, weight):
+        if(weight.shape[0] != self.inDim or weight.shape[1]!= self.outDim):
+            return None
+        else:
+            self.weight = weight
+            return self
+    def activeNode(self, nodeIndex):
+        self.livelyNode = nodeIndex
+        return self
+    def input(self, data):
+        if(data.shape[1] != self.inDim):
+            return None
+        else:
+            self.X = data
+            self.size = data.shape[0]
+            return self
+    def forwardPropogation(self):
+        self.Y = self.X.dot(self.weight[:,self.livelyNode])
+        return self
+    def output(self):
+        return self.Y
+    def setDy(self, dy):
+        if(dy.shape[0] != self.size or dy.shape[1]!= self.outDim):
+            return None
+        else:
+            self.dy = dy
+            return self
+    def backwardPropogation(self,s):
+        self.dw = self.X.T.dot(self.dy)
+        self.dx = self.dy.dot(self.weight.T)
+        self.weight = self.weight - self.learning_rate * self.dw
+        return self
+    def getDx(self):
+        return self.dx
+    def getWeight(self):
+        return self.weight
 class model:
-    def __init__(self, x, y, layer):
-        self.layersNumber = len(layer)
-        self.layer = layer
+    def __init__(self):
+        self.layers = []
+        return 
+    def addLayer(self,layer):
+        self.layers.append(layer)
+        if(len(self.layers) == 1):
+            self.n = 1
+            self.i = 1
+        else:
+            self.n += 1
+        return self
+    def fit0(self, X, Y):
+        self.size = X.shape[0]
 
-        self.data = [None for i in layer]
-        self.data[0] = np.matrix(x).T
+        y = self.forwardPropogation(X)
+        r = Y-y
+        print(r.T.dot(r)/self.size)
 
-        self.trueData = [None for i in range(self.layersNumber)]
-        self.trueData[(self.layersNumber)-1] = np.matrix(y).T
+        dy = 2*r/self.size
+        self.i = self.i-1
+        while(self.i < 1):
+            dy = self.layers[self.i].setDy(dy).backwardPropogation(4).getDx()
+            self.i = self.i-1
+        self.i = 1
 
-        self.weight = [[None for j in range(layer[i+1])] for i in range(self.layersNumber - 1)]
+    def predict(self, X_prediction):
+        return self.forwardPropogation(X_prediction)
 
-    def gweight(self, initializer):
-        for i in range(self.layersNumber - 1):
-            matrix = initializer(shape = (self.layer[i], self.layer[i+1])).numpy().T
-            for j, row in enumerate(matrix):
-                self.weight[i][j] = np.matrix(row)
-    
-    def forward(self):
-        for i, weight in enumerate(self.weight):
-            data=[]
-            for j, w in enumerate(weight):
-                data.append((w * self.data[i])[0,0])
-            self.data[i+1] = np.matrix(data).T
-        diff = self.data[self.layersNumber-1] - self.trueData[(self.layersNumber)-1]
-        self.Loss = diff * (diff.T)
-
-    def backward(self, learning_rate):
-        d = 2 * (self.data[self.layersNumber-1] - self.trueData[self.layersNumber-1])
-        for i in range(self.layersNumber-1, 0 ,-1):
-            if(i == 2):
-                d1 = d * self.weight[1][0]
-            else:
-                d1 = None
-            for j, w in enumerate(self.weight[i-1]):                
-                self.weight[i-1][j]  = w - learning_rate * d[0, j] *(self.data[i-1].T)
-            d = d1
-
-m = model([1,2,3,4], 4, [4, 100, 1])
-initializer = keras.initializers.RandomNormal(mean=0., stddev=1., seed = 1234)
-#m.weight = [[np.matrix([1,2,3]), np.matrix([4,5,6])],[np.matrix([7,8])]]
-m.gweight(initializer)
-m.forward()
-m.backward(0.1)
-
-print(np.array([i.tolist()[0] for i in m.weight[0]]))
-print(np.array([i.tolist()[0] for i in m.weight[1]]))
+        
+    def forwardPropogation(self, X):
+        y = self.layers[0].input(X).forwardPropogation().output()
+        while(self.i < self.n):
+            y = self.layers[self.i].input(y).forwardPropogation().output()
+            self.i = self.i+1
+        return y
--- kerasVal.py
+++ kerasVal.py
@@ -6,20 +6,25 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.losses import MeanSquaredError
 
+
 initializer = keras.initializers.RandomNormal(mean=0., stddev=1., seed = 1234)
 
-X_train1 = np.array([[1,2,3,4]])
-y_train1 = np.array([[4]])
+X_train1 = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+y_train1 = np.array([[23], [24],[25],[26]])
 
 model1 = Sequential()
-model1.add(Dense(100, input_dim=4, kernel_initializer=initializer, use_bias=False))
+model1.add(Dense(4, input_dim=3, kernel_initializer=initializer, use_bias=False))
+model1.add(Dense(5, kernel_initializer=initializer, use_bias=False))
 model1.add(Dense(1, kernel_initializer=initializer, use_bias=False))
 model1.compile(loss=MeanSquaredError(), optimizer=keras.optimizers.SGD(learning_rate=0.1))
 
-#model1.layers[0].set_weights([np.array([[1,4],[2,5],[3,6]])])
-#model1.layers[1].set_weights([np.array([[7],[8]])])
+model1.layers[0].set_weights([np.array([[7,8,9,10],[11,12,13,14],[15,16,17,18]])])
+model1.layers[1].set_weights([np.array([[19,20,21,22,23],[23,24,25,26,27],[28,29,30,31,32],[33,34,35,36,37]])])
+model1.layers[2].set_weights([np.array([[37],[38],[39],[40],[41]])])
 
 model1.fit(X_train1, y_train1, epochs=1)
 
-print(model1.layers[0].get_weights()[0].T)
-print(model1.layers[1].get_weights()[0].T)
+#print(model1.layers[0].get_weights()[0].T)
+#print(model1.layers[1].get_weights()[0].T)
+p = model1.predict(np.array([[13,14,15]]))
+print(p)
