25111332f54ae3e6ef02bfa7059c9d54843810c5
Winter-Shen <47650619+Winter-Shen@users.noreply.github.com>
2023-03-27 17:12:13 +1300

    code

--- debug.py
+++ debug.py
@@ -11,6 +11,10 @@ X_train = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
 y_train = np.array([[23,24], [25,26],[27,28],[29,30]])
 X_test = np.array([[13,14,15]])
 
+w1 = np.array([[7,8,9,10],[11,12,13,14],[15,16,17,18]])
+w2 = np.array([[19,20,21,22,23],[23,24,25,26,27],[28,29,30,31,32],[33,34,35,36,37]])
+w3 = np.array([[37,38],[39,40],[41,42],[43,44],[45,46]])
+
 '''
 My Network
 '''
@@ -19,13 +23,9 @@ initializer = RandomNormal(mean=0., stddev=1., seed = 12345)
 m1 = nn.MyModel()
 m1.addLayer(nn.InputLayer(3))
 
-#m.addLayer(nn.MyLayer(3, 4).initializeWeight(initializer))
-#m.addLayer(nn.MyLayer(4, 5).initializeWeight(initializer))
-#m.addLayer(nn.MyLayer(5, 2).initializeWeight(initializer))
-
-m1.addLayer(nn.MyLayer(3, 4).setWeight(np.array([[7,8,9,10],[11,12,13,14],[15,16,17,18]])))
-m1.addLayer(nn.MyLayer(4, 5).setWeight(np.array([[19,20,21,22,23],[23,24,25,26,27],[28,29,30,31,32],[33,34,35,36,37]])))
-m1.addLayer(nn.MyLayer(5, 2).setWeight(np.array([[37,38],[39,40],[41,42],[43,44],[45,46]])))
+m1.addLayer(nn.MyLayer(3, 4).setWeight(w1))
+m1.addLayer(nn.MyLayer(4, 5).setWeight(w2))
+m1.addLayer(nn.MyLayer(5, 2).setWeight(w3))
 
 #m.selectNodes([[1,3],[0,2,4]])
 m1.fit(X_train, y_train, 0.1, epochs=1)
@@ -40,16 +40,24 @@ Keras Network
 initializer = RandomNormal(mean=0., stddev=1., seed = 12345)
 
 m2 = Sequential()
-m2.add(Dense(4, input_dim=3, kernel_initializer=initializer, use_bias=False))
-m2.add(Dense(5, kernel_initializer=initializer, use_bias=False))
-m2.add(Dense(2, kernel_initializer=initializer, use_bias=False))
+m2.add(Dense(4, input_dim=3, use_bias=False))
+m2.add(Dense(5, use_bias=False))
+m2.add(Dense(2, use_bias=False))
 m2.compile(loss=MeanSquaredError(), optimizer=SGD(learning_rate=0.1))
 
-m2.layers[0].set_weights([np.array([[7,8,9,10],[11,12,13,14],[15,16,17,18]])])
-m2.layers[1].set_weights([np.array([[19,20,21,22,23],[23,24,25,26,27],[28,29,30,31,32],[33,34,35,36,37]])])
-m2.layers[2].set_weights([np.array([[37,38],[39,40],[41,42],[43,44],[45,46]])])
+print(m2.layers[0])
+
+
+
+m2.layers[0].set_weights([w1])
+m2.layers[1].set_weights([w2])
+m2.layers[2].set_weights([w3])
+
+
+print(m2.layers[0].get_weights())
 
 m2.fit(X_train, y_train, epochs=1)
+#print(m2.layers[0].get_weights())
 
-p = m2.predict(X_test)
-print(p)
+y_hat = m2.predict(X_test)
+#print(y_hat)
