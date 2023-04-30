ebda86b17fef8be9dcd32b6f955a057651341acd
Wentao <wshe801@aucklanduni.ac.nz>
2023-04-30 17:06:06 +1200

    code

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -4,7 +4,98 @@ from keras.metrics import CategoricalAccuracy
 from tqdm import tqdm
 from keras.initializers import GlorotUniform
 
+
 class MyLayer:
+    def __init__(self, in_dim, out_dim):
+        self.inDim = in_dim
+        self.outDim = out_dim
+        self.initializeWeight(GlorotUniform())
+    def initializeWeight(self, initializer):
+        return self.setWeight(initializer((self.inDim, self.outDim)).numpy())
+    def setWeight(self, weight):
+        if(weight.shape[0] != self.inDim or weight.shape[1]!= self.outDim):
+            return None
+        else:
+            self.weight = weight.astype(np.float64)
+            return self
+    def setLearningRate(self, learning_rate):
+        self.learningRate = learning_rate
+    def forwardPropagation(self, x):
+        self.x = x.astype(np.float64)
+        return self.x.dot(self.weight)
+    def backwardPropagation(self,dy):
+        self.dw = self.x.T.dot(dy)
+        dx = dy.dot(self.weight.T)
+        self.weight = self.weight - self.learningRate * self.dw
+        return dx
+    def getWeight(self):
+        return self.weight
+
+class dropout:
+    def __init__(self, rate):
+        self.rate = rate
+    def forwardPropagation(self, x):
+        k = np.random.rand(x.shape[1])
+        self.mask = k > self.rate
+        return (x*self.mask)/(1-self.rate)
+    def backwardPropagation(self, dy):
+        return (dy * self.mask)/(1-self.rate)
+
+class MyModel:
+    def __init__(self):
+        self.layers = []
+        self.dataLayer = []
+        return 
+    def addLayer(self,layer):
+        self.layers.append(layer)
+        self.n = 1 if(len(self.layers) == 1) else self.n+1
+        if(isinstance(layer, MyLayer)):
+            self.dataLayer.append(self.n-1)
+        return self
+    def fit(self, X, Y, learning_rate, epoches, batch_size):
+        metrics = CategoricalAccuracy()
+        #Generate Batches
+        batches = self.getBatches(X, Y, batch_size)
+        l = len(batches)
+
+        for i in self.dataLayer:
+            self.layers[i].setLearningRate(learning_rate)
+
+        for i in range(epoches):
+            batchesP = tqdm(range(l), bar_format="{l_bar}")
+            cm = 0
+            for j in batchesP:
+                [loss, accuracy] = self.fit0(batches[j][0], batches[j][1])
+                cm = (cm * j+accuracy)/(j+1)
+                batchesP.set_description("Epoch %2d: %3d/%3d; Accuracy: %.4f; Loss: %.4f" % (i+1, j+1, l, cm, loss))
+    def fit0(self, x, y):
+        # Forwardpropogation
+        for l in self.layers:
+            x = l.forwardPropagation(x)
+        y_hat = x
+        mse = MeanSquaredError()
+        metrics = CategoricalAccuracy()
+        loss = mse(y, y_hat).numpy()
+        metrics.update_state(y, y_hat)
+        accuracy = metrics.result().numpy()
+
+        r = y_hat-y
+        dy = 2*r/((r.shape[0])*(r.shape[1]))
+        # backwardpropogation
+        for l in range(self.n-1, -1, -1):
+            dy = self.layers[l].backwardPropagation(dy)
+        return([loss, accuracy])
+    def getBatches(self, X, Y, batch_size):
+        indices = np.arange(len(X))
+        batches = [(X[indices[i:i+batch_size]], Y[indices[i:i+batch_size]]) for i in range(0, len(X), batch_size)]
+        return batches
+    def predict(self, x):
+        for i in self.dataLayer:
+            x = self.layers[i].forwardPropagation(x)
+        return x
+
+'''
+class MyLayer1:
     def __init__(self, inDim, outDim):
         self.inDim = inDim
         self.outDim = outDim
@@ -118,3 +209,4 @@ class MyModel:
         #np.random.shuffle(indices)
         batches = [(X[indices[i:i+batch_size]], Y[indices[i:i+batch_size]]) for i in range(0, len(X), batch_size)]
         return batches
+'''
