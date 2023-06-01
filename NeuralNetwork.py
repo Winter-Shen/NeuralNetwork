a556ba5f931d1d38ba7a9f1873898460ee14f5d6
Wentao Shen <wshe801@aucklanduni.ac.nz>
2023-06-01 17:39:18 +1200

    code

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -23,7 +23,7 @@ class MyLayer:
 
             self.hash_tables = [[set()]*self.buckect_num]*table_num # Hash tables 
             self.hash_values = [[0]*out_dim]*table_num # Lists to hold hash values to avoid repeating computing hash values
-            self.projections = [np.random.randn(in_dim, function_num)]*table_num # Projection vector, the component of hash function
+            self.projections = [np.random.randn(in_dim + 1, function_num)]*table_num # Projection vector, the component of hash function
 
             self.__constructHashTable()
         
@@ -31,26 +31,23 @@ class MyLayer:
         self.mask_X = np.full((n,self.out_dim), 0)
         self.batch_size = np.append(np.full(n//batch_size, batch_size), n%batch_size)
         self.computed_mask_batch_flag = np.zeros(n//batch_size + 1, dtype=bool)
+
     def dropoutConfiguration(self):
-        code = 0
-        code = code^(self.dropout<<1)
-        code = code^(self.dropout_lsh)
-        return code
+        return 0^(self.dropout<<1)^(self.dropout_lsh)
+
     # Initialize weight matrix by a initializer of keras.initializers
     def initializeWeight(self, initializer):
         return self.setWeight(initializer((self.in_dim, self.out_dim)).numpy())
+
     # Initialize weight matrix by a numpy array
     def setWeight(self, weight):
-        if(weight.shape[0] != self.in_dim or weight.shape[1]!= self.out_dim):
-            return None
-        else:
-            self.weight = weight.astype(np.float64)
-            return self
-    def __fixWeight(self):
-        self.squared_norm = np.array([np.dot(w, w.T) for w in self.weight.T], dtype=np.float64)
+        self.weight = weight.astype(np.float64)
+        return self
+
     # Set learning rate
     def setLearningRate(self, learning_rate):
         self.learning_rate = learning_rate 
+
     # Take input set x and return output set after forward propagation
     def forwardPropagation(self, x, batch_label = -1, prediction = False):
         self.batch = x.astype(np.float64)
@@ -72,6 +69,7 @@ class MyLayer:
         else:
             return self.batch.dot(self.weight)
         return (self.batch.dot(self.weight)*self.mask)/self.rate
+
     # Take derivatives of output set and return derivatives of inputset set after backward propagation
     def backwardPropagation(self,dy):
         if self.rate is not None:
@@ -82,79 +80,113 @@ class MyLayer:
         if(self.dropout_lsh):
             self.__updateHashTables()
         return dx
-    def __constructHashTable(self):
-        for idx, w in enumerate(self.weight.T):
 
-            for i in range(self.table_num):
-                hash_value = self.__computeFingerprint(w, self.projections[i])
-                self.hash_values[i][idx] = hash_value
-                self.hash_tables[i][hash_value].add(idx)
     def __collectActiveSet(self, batch_label):
         if(batch_label != -1): # Input for this training cycle is a batch
-            idx_range = list(range(batch_label,batch_label+self.batch_size[batch_label]))
+
+            idx_range = list(range(batch_label*(self.batch_size[batch_label]),batch_label*(self.batch_size[batch_label])+self.batch_size[batch_label]))
+
             if(self.computed_mask_batch_flag[batch_label]): # The mask vectors have alraedy been computed.
                 self.mask = self.mask_X[idx_range]
             else: # The mask vectors have not been computed.
                 self.mask = np.zeros((self.batch_size[batch_label],self.out_dim), dtype=bool)
                 for i, x in enumerate(self.batch):
+                    
+                    # Fix x
+                    norm = np.sqrt(np.dot(x, x.T))
+                    new_x = np.append(x/norm, 0)
+
                     acitive_set = set()
                     for t,projection in enumerate(self.projections):
-                        fp = self.__computeFingerprint(x, projection, weight_vecotr = False)
+                        fp = self.__computeFingerprint(new_x, projection)
                         acitive_set = self.hash_tables[t][fp] | acitive_set
                     self.mask[i][list(acitive_set)] = True
+
                 self.mask_X[idx_range] = self.mask
                 self.computed_mask_batch_flag[batch_label] = True
-        else: # Input for this training cycle is the complete X. Often run when prediction and all the mask vectors should be computed 
+        else: # Input for this training cycle is the complete X. Often run when it is prediction and all the mask vectors should be computed 
             self.mask = np.zeros((self.batch.shape[0],self.out_dim), dtype=bool)
             for i, x in enumerate(self.batch):
+
+                # Fix x
+                norm = np.sqrt(np.dot(x, x.T))
+                new_x = np.append(x/norm, 0)
+
                 acitive_set = set()
                 for t,projection in enumerate(self.projections):
-                    fp = self.__computeFingerprint(x, projection, weight_vecotr = False)
+                    fp = self.__computeFingerprint(new_x, projection)
                     acitive_set = self.hash_tables[t][fp] | acitive_set
                 self.mask[i][list(acitive_set)] = True
-    def __updateHashTables(self):
-        for idx, w in enumerate(self.weight.T):
-            for i in range(self.table_num):
-                self.hash_tables[i][self.hash_values[i][idx]].remove(idx)
 
-                hash_value = self.__computeFingerprint(w, self.projections[i])        
+    def __constructHashTable(self):
+        self.__maxNormWeight()
+        for idx, w in enumerate(self.weight.T):
+
+            for i in range(self.table_num):
+
+                # Fix weight vector
+                weight = w/self.max_norm
+                norm = self.norm[idx]
+                e = np.sqrt(1-(norm/self.max_norm)**2)
+                new_w = np.append(weight, e)
+                
+                hash_value = self.__computeFingerprint(new_w, self.projections[i])
                 self.hash_values[i][idx] = hash_value
                 self.hash_tables[i][hash_value].add(idx)
 
-    def __computeFingerprint(self, vector, projection, weight_vecotr = True):
-        '''
-        if(weight_vecotr):
-            vector = vector/self.max_norm
-            square_norm = np.dot(vector, vector.T)
-            e = np.sqrt(1-square_norm/self.max_norm)
-            fixed_vector = np.append(vector, e)
-        else:
-            norm = np.sqrt(np.dot(vector, vector.T))
-            fixed_vector = np.append(vector, norm)
-        '''
+    def __updateHashTables(self):
+        self.__maxNormWeight()
+        for idx, w in enumerate(self.weight.T):
+            for i in range(self.table_num):
+
+                # Fix weight vector
+                weight = w/self.max_norm
+                norm = self.norm[idx]
+                e = np.sqrt(1-(norm/self.max_norm)**2)
+                new_w = np.append(weight, e)
+
+                # Remove the weight connection in the hash table
+                self.hash_tables[i][self.hash_values[i][idx]].remove(idx)
+
+                # Compute hash value and update the weight connection
+                hash_value = self.__computeFingerprint(new_w, self.projections[i])        
+                self.hash_values[i][idx] = hash_value
+                self.hash_tables[i][hash_value].add(idx)
+
+    def __maxNormWeight(self):
+        self.norm = np.array([np.sqrt(np.dot(w, w.T)) for w in self.weight.T], dtype=np.float64)
+        self.max_norm = np.max(self.norm)   
+
+
+    def __computeFingerprint(self, vector, projection):
+
         hash_vector = (np.dot(vector, projection) > 0).astype('int8')
         hash_value = 0
         for b, s in enumerate(hash_vector):
             hash_value = hash_value^(s<<b)
         return hash_value
+
     def __computeFingerprints(self, vector):
         return [self.__computeFingerprint(vector, projection) for projection in self.projections]
+
     def reset(self):
         self.initializeWeight(GlorotUniform())
         if(self.dropout_lsh):
             self.hash_tables = [[set()]*self.buckect_num]*self.table_num
             self.hash_values = [[0]*self.out_dim]*self.table_num
-            self.projections = [np.random.randn(self.in_dim, self.function_num)]*self.table_num
+            self.projections = [np.random.randn(self.in_dim + 1, self.function_num)]*self.table_num
             self.__constructHashTable()
 
 class MyModel:
     def __init__(self):
         self.layers = []
         return 
+
     def addLayer(self,layer):
         self.layers.append(layer)
         self.n = 1 if(len(self.layers) == 1) else self.n+1
         return self
+
     # A forward and backward cycle
     def __fit(self, x, y, batch_label):
         # Forwardpropogation
@@ -174,6 +206,7 @@ class MyModel:
         for l in range(self.n-1, -1, -1):
             dy = self.layers[l].backwardPropagation(dy)
         return([loss, accuracy])
+
     def fit(self, X, Y, learning_rate, epoches, batch_size, progress = True):
         #Generate Batches
         batches = self.__getBatches(X, Y, batch_size)
@@ -192,13 +225,16 @@ class MyModel:
                 cm = (cm * j+accuracy)/(j+1)
                 batchesP.set_description("Epoch %2d" % (i+1))
                 batchesP.set_postfix_str("Accuracy: %.4f, Loss: %.4f;" % (round(cm, 4), round(loss, 4)))
+
     def predict(self, x):
         for l in self.layers:
             x = l.forwardPropagation(x, prediction = True)
         return x
+
     def reset(self):
         for l in self.layers:
             l.reset()
+
     def __getBatches(self, X, Y, batch_size):
         indices = np.arange(len(X))
         batches = [(X[indices[i:i+batch_size]], Y[indices[i:i+batch_size]]) for i in range(0, len(X), batch_size)]
